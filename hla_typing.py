8e57831ffd4e2a185ffcaa89b2f7497fc2aa86e9
naser <elmi@cimorgh.ir>
2024-06-12 12:05:11 +0000

    revised

--- hla_typing.py
+++ hla_typing.py
@@ -1,39 +1,29 @@
 import os
 import argparse
 import subprocess
+import pandas as pd
 
-# List of HLA gene names
-hla_genes = ["HLA-A", "HLA-B", "HLA-C", "HLA-E", "HLA-F", "HLA-G", 
-             "HLA-DRB1", "HLA-DRB3", "HLA-DRB4", "HLA-DRB5", 
-             "HLA-DQA1", "HLA-DQB1", "HLA-DPA1", "HLA-DPB1",
-             "HLA-DMA","HLA-DMB","HLA-DOA","HLA-DOB","HLA-DPB2",
-             "HLA-DQB2","HLA-DQA2","HLA-DRA","HLA-DRB6","HLA-F-AS1",
-             "HLA-G","HLA-H","HLA-J","HLA-L","HLA-V"]
+def read_hla_genes(genes_list_file):
+    hla_genes = []
+    with open(genes_list_file, 'r') as f:
+        for line in f:
+            gene_name = line.strip().split('\t')[0]
+            hla_genes.append(gene_name)
+    return hla_genes
 
-# Define function to convert Ensembl GTF to BED format with padding
-def gtf_to_bed(input_file, exon_output_file, padding):
+def gtf_to_bed(input_file, exon_output_file, padding, hla_genes):
     with open(input_file, 'r') as f_in, open(exon_output_file, 'w') as f_out:
         for line in f_in:
-            # Skip comments
             if line.startswith('#'):
                 continue
-            
-            # Split line into fields
             fields = line.strip().split('\t')
-            
-            # Extract relevant fields
-            chrom = fields[0].replace('chr', '')  # Remove 'chr' prefix if present
+            chrom = fields[0].replace('chr', '')
             feature = fields[2]
-            start = int(fields[3]) - 1  # Convert to 0-based
+            start = int(fields[3]) - 1
             end = int(fields[4])
             strand = fields[6]
-            
-            # Extract gene_id, exon_number, and gene_name from the attributes field
             attributes = fields[8]
-            gene_id = None
-            exon_number = None
-            gene_name = None
-            transcript_id = None
+            gene_id, exon_number, gene_name, transcript_id = None, None, None, None
             for attribute in attributes.split(';'):
                 attribute = attribute.strip()
                 if attribute.startswith('gene_id'):
@@ -44,25 +34,17 @@ def gtf_to_bed(input_file, exon_output_file, padding):
                     gene_name = attribute.split(' ')[1].replace('"', '')
                 elif attribute.startswith('transcript_id'):
                     transcript_id = attribute.split(' ')[1].replace('"','')
-            
-            # Check if the feature is an exon
-            if gene_name in hla_genes and gene_id and exon_number and '_' not in chrom:
-                # Adjust start and end positions by padding
-                padded_start = max(start - padding, 0)  # Ensure start is not negative
+            if gene_name in hla_genes and gene_id and exon_number and '_' not in chrom and (transcript_id.startswith("NM_") or transcript_id.startswith("ENST")):
+                padded_start = max(start - padding, 0)
                 padded_end = end + padding
-                
-                # Write BED-formatted line with strand, gene_id, and exon_number
                 bed_line = f"{chrom}\t{padded_start}\t{padded_end}\t{feature}\t{strand}\t{gene_name}\t{transcript_id}\t{exon_number}\n"
                 f_out.write(bed_line)
 
 def add_exon_ids_and_sort(exon_output_file, sorted_exon_output_file):
-    # Sort the exon-level BED file using the sort command
-    subprocess.run(f"sort -k6,6V -k1,1V -k2,2n -k3,3n {exon_output_file} | uniq > {sorted_exon_output_file}", shell=True, check=True)
+    subprocess.run(f"sort -k1,1V -k2,2n -k3,3n {exon_output_file} | uniq > {sorted_exon_output_file}", shell=True, check=True)
 
 def generate_gene_level_bed(sorted_exon_output_file, gene_output_file):
     gene_dict = {}
-
-    # Read the sorted exon-level BED file
     with open(sorted_exon_output_file, 'r') as f_in:
         for line in f_in:
             fields = line.strip().split('\t')
@@ -71,15 +53,11 @@ def generate_gene_level_bed(sorted_exon_output_file, gene_output_file):
             end = int(fields[2])
             strand = fields[4]
             gene_name = fields[5]
-
-            # Update gene start and end positions
             if gene_name not in gene_dict:
                 gene_dict[gene_name] = [chrom, start, end, strand]
             else:
                 gene_dict[gene_name][1] = min(gene_dict[gene_name][1], start)
                 gene_dict[gene_name][2] = max(gene_dict[gene_name][2], end)
-    
-    # Write the gene-level BED file
     with open(gene_output_file, 'w') as f_out:
         for gene_name, values in gene_dict.items():
             chrom, start, end, strand = values
@@ -87,7 +65,6 @@ def generate_gene_level_bed(sorted_exon_output_file, gene_output_file):
             f_out.write(bed_line)
 
 def sort_gene_level_bed(gene_output_file, sorted_gene_output_file):
-    # Sort the gene-level BED file using the sort command
     subprocess.run(f"sort -k4,4V -k1,1V -k2,2n -k3,3n {gene_output_file} > {sorted_gene_output_file}", shell=True, check=True)
     os.remove(gene_output_file)
 
@@ -97,13 +74,10 @@ def generate_filtered_sorted_exon_bed(sorted_exon_output_file, filtered_exon_out
             fields = line.strip().split('\t')
             feature = fields[3]
             transcript_id = fields[6]
-            
-            if feature == "CDS" and transcript_id.startswith("NM_"):
+            if feature == "exon" and (transcript_id.startswith("NM_") or transcript_id.startswith("ENST")):
                 f_out.write(line)
-    
-    # Sort the filtered exon-level BED file using the sort command
     sorted_filtered_exon_output_file = filtered_exon_output_file.replace('.bed', '_sorted.bed')
-    subprocess.run(f"sort -k6,6V -k1,1V -k2,2n -k3,3n {filtered_exon_output_file} > {sorted_filtered_exon_output_file}", shell=True, check=True)
+    subprocess.run(f"sort -k1,1V -k2,2n -k3,3n {filtered_exon_output_file} > {sorted_filtered_exon_output_file}", shell=True, check=True)
     os.remove(filtered_exon_output_file)
     return sorted_filtered_exon_output_file
 
@@ -112,7 +86,7 @@ def main():
     parser.add_argument('input_file', type=str, help='Path to the input Ensembl GTF file')
     parser.add_argument('output_prefix', type=str, help='Prefix for the output BED files')
     parser.add_argument('padding', type=int, help='Padding distance to add to exon start and end positions')
-    
+    parser.add_argument('genes_list_file', type=str, help='Path to the panel genes list file')
     args = parser.parse_args()
 
     exon_output_file = f"{args.output_prefix}_exon_level.bed"
@@ -121,28 +95,31 @@ def main():
     sorted_gene_output_file = f"{args.output_prefix}_sorted_gene_level.bed"
     filtered_exon_output_file = f"{args.output_prefix}_filtered_exon_level.bed"
 
-    # Convert Ensembl GTF to exon-level BED format with padding
-    gtf_to_bed(args.input_file, exon_output_file, args.padding)
-    
-    # Sort the exon-level BED file and generate sorted exon-level BED file
-    add_exon_ids_and_sort(exon_output_file, sorted_exon_output_file)
-    
-    # Generate gene-level BED file
-    generate_gene_level_bed(sorted_exon_output_file, gene_output_file)
-    
-    # Sort the gene-level BED file and generate sorted gene-level BED file
-    sort_gene_level_bed(gene_output_file, sorted_gene_output_file)
+    hla_genes = read_hla_genes(args.genes_list_file)
 
-    # Generate filtered and sorted exon-level BED file
+    gtf_to_bed(args.input_file, exon_output_file, args.padding, hla_genes)
+    add_exon_ids_and_sort(exon_output_file, sorted_exon_output_file)
+    generate_gene_level_bed(sorted_exon_output_file, gene_output_file)
+    sort_gene_level_bed(gene_output_file, sorted_gene_output_file)
     sorted_filtered_exon_output_file = generate_filtered_sorted_exon_bed(sorted_exon_output_file, filtered_exon_output_file)
 
-    # Remove temporary files
+    df = pd.read_csv(sorted_filtered_exon_output_file, sep='\t', header=None)
+    df_unique = df.drop_duplicates(subset=[0, 1, 2, 3, 4, 5])
+    unique_exon_level_output = f"{args.output_prefix}_unique_exon_level.bed"
+    df_unique.to_csv(unique_exon_level_output, sep='\t', index=False, header=False)
+
+    # Sort the final unique exon-level BED file
+    final_sorted_exon_output_file = f"{args.output_prefix}_final_sorted_exon_level.bed"
+    subprocess.run(f"sort -k6,6V -k1,1V -k2,2n -k3,3n {unique_exon_level_output} > {final_sorted_exon_output_file}", shell=True, check=True)
+
     os.remove(exon_output_file)
     os.remove(sorted_exon_output_file)
+    os.remove(sorted_filtered_exon_output_file)
+    os.remove(unique_exon_level_output)
 
     print("Sorted exon-level BED file created successfully.")
     print("Sorted gene-level BED file created successfully.")
-    print(f"Filtered and sorted exon-level BED file created successfully: {sorted_filtered_exon_output_file}")
+    print(f"Filtered, unique, and sorted exon-level BED file created successfully: {final_sorted_exon_output_file}")
 
 if __name__ == '__main__':
     main()
